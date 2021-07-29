a26ea18e04f4ba10ea6989be2fb8e7976fdcbde6
mstrivens <max.strivens@live.co.uk>
2021-07-29 15:40:35 +0100

    Player initializes with starting position of 0.

--- player.test.js
+++ player.test.js
@@ -5,4 +5,8 @@ describe('player initializes with name and player position of 0', () => {
   test('new player object has a name', () => {
     expect(player.name).toEqual('Max')
   })
+
+  test('new player object has a position of 0', () => {
+    expect(player.position).toEqual(0)
+  })
 })
